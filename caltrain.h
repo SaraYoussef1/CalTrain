329ba31852b7e418d01f0819be4bf49d226b986b
SaraYoussef1 <doctorsara94@gmail.com>
2021-03-30 21:44:58 +0200

    locked area reduced

---
+++ caltrain.h
@@ -0,0 +1,20 @@
+#include <pthread.h>
+
+struct station {
+
+    pthread_mutex_t mutex;         //control critical section
+    pthread_cond_t arrival;        //signal when train arrives station
+    pthread_cond_t leaving;        //signal when train is ready to leave
+
+    int seats;                      //available seats
+    int waiting_passengers;         //passengers on station
+    int leaving_passengers;         //passengers on board
+};
+
+void station_init(struct station *station);
+
+void station_load_train(struct station *station, int count);
+
+void station_wait_for_train(struct station *station);
+
+void station_on_board(struct station *station);
